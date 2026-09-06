ab598ae6414ba79b5ffa139b9b32e08d2ebf2ef4
JanayedHossain <junayedhossain603@gmail.com>
2026-09-06 19:46:47 +0600

    Answer 2

--- answer.js
+++ answer.js
@@ -9,3 +9,27 @@ function describeValue(value) {
         return type + " | falsy";
     }
 }
+
+
+// Question 2
+
+function getDayType(day) {
+    day = day.toLowerCase();
+
+    switch (day) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
+    }
+}
+
